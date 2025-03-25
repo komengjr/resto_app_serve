3a413ea97720c94f6e690c069c4cba43891285b2
komengjr <47206605+komengjr@users.noreply.github.com>
2025-03-25 11:38:59 +0700

    updateakses2

--- resources/views/master/menu-akses.blade.php
+++ resources/views/master/menu-akses.blade.php
@@ -57,7 +57,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-8 col-xxl-9">
+        <div class="col-xl-8">
             {{-- <div class="card mb-3">
                 <div class="card-body">
                     <div class="row">
